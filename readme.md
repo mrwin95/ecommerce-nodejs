daf579505e683a41058e801dd544b4b941184cd1
Thang Nguyen <mrwin95@gmail.com>
2024-09-21 03:25:45 -0600

    added middleware compression

--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 how to use pnpm
 macOS:
+https://pnpm.io/installation
 - install corepack brew install corepack
 - enable pnpm corepack enable pnpm
 - pin the version corepack use pnpm@latest
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import helmet from "helmet";
+import compression from "compression";
+
 import morgan from "morgan";
 const app = express();
 
@@ -8,6 +10,7 @@ export default app;
 // load middleware
 app.use(morgan("dev"));
 app.use(helmet());
+app.use(compression());
 // app.use(morgan("combined"));
 // app.use(morgan("common"));
 // app.use(morgan("short"));
@@ -17,8 +20,11 @@ app.use(helmet());
 // load route
 
 app.get("/", (req, res, next) => {
+  const strCompress = "Hello would";
+
   return res.status(200).json({
     message: "Test morgan",
+    metadata: strCompress.repeat(1000),
   });
 });
 
